6d3bee4a0a5fa2ea637ab12f13049d24758580a1
Oliver Scotten <revi99@me.com>
2024-03-19 13:52:23 +0000

    --env-var Flag Created

--- shorturl/runner.py
+++ shorturl/runner.py
@@ -11,9 +11,7 @@ def run():
     PARSER.add_argument('--host', nargs='?', default='127.0.0.1', type=str, help='host listening address')
     PARSER.add_argument('--port', nargs='?', default=5000, type=int, help='host listening port')
 
-    # Add this functionalty at a later date.
-    # PARSER.add_argument('--dbhost', nargs='?', default='redis', type=str, help='redis database host listening address')
-    # PARSER.add_argument('--dbport', nargs='?', default=6379, type=int, help='redis database host listening port')
+    PARSER.add_argument('--env-file', default='.env', type=str, help='path to .env file')
 
     PARSER.add_argument('--reload', action='store_true', help=argparse.SUPPRESS)
     PARSER.add_argument('--test', action='store_true', help=argparse.SUPPRESS)
@@ -27,4 +25,4 @@ def run():
     TEST = parsedArguments.test
 
     if not TEST:
-        uvicorn.run("shorturl.app:app", host=HOST, port=PORT, log_level="info", reload=RELOAD)
+        uvicorn.run("shorturl.app:app", host=HOST, port=PORT, log_level="info", reload=RELOAD, env_file='.env')
